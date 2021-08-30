9a258db6a70b497a45a5b738256dbcb49b68223f
Thiago Lanza <lanzathm@gmail.com>
2021-08-30 16:53:46 -0300

    Example of component test with comments

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,25 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UniqueIdService } from '../../services/unique-id/unique-id.service';
 import { LikeWidgetComponent } from './like-widget.component';
 
-describe('LikeWidgetComponent', () => {
-  let component: LikeWidgetComponent;
-  let fixture: ComponentFixture<LikeWidgetComponent>;
+describe(LikeWidgetComponent.name, () => {
+  // Wrapper que terá a instância do componente e métodos auxiliares para testes.
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
 
   beforeEach(async () => {
+    // Cria e configura um módulo de teste com os mesmos parâmetros do módulo do componente
+    // compileComponents é um método assíncrono que retorna uma Promise
+    // faz-se necessário o uso do async/await
     await TestBed.configureTestingModule({
-      declarations: [ LikeWidgetComponent ]
-    })
-    .compileComponents();
-  });
+      declarations: [LikeWidgetComponent],
+      imports: [FontAwesomeModule],
+      providers: [UniqueIdService],
+    }).compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(LikeWidgetComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Should create component', () => {
+    const instance = fixture.componentInstance;
+
+    // Caso o componente não tenha sido criado, será falso o expect.
+    expect(instance).toBeTruthy();
   });
 });
